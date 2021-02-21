817511ea3e3920990bb8adf33dc5cfdeca984acd
Boßmann Daniel <dbossmann97@gmail.com>
2021-02-21 17:43:38 +0100

    Views hinzugefügt

---
+++ implementierung/buchhaltung/buchhaltung/Views/DataGrid.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Shell.Views
+{
+    /// <summary>
+    /// Interaction logic for DataGrid.xaml
+    /// </summary>
+    public partial class DataGrid : UserControl
+    {
+        public DataGrid()
+        {
+            InitializeComponent();
+        }
+    }
+}
