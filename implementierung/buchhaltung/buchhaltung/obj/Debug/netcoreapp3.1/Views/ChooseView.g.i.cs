ccea38ef66a762cd5e18e72bdcf2d550c1aa3caf
Boßmann Daniel <dbossmann97@gmail.com>
2021-02-21 19:18:07 +0100

    Calculation View erstellt

--- implementierung/buchhaltung/buchhaltung/obj/Debug/netcoreapp3.1/Views/ChooseView.g.i.cs
+++ implementierung/buchhaltung/buchhaltung/obj/Debug/netcoreapp3.1/Views/ChooseView.g.i.cs
@@ -55,7 +55,7 @@ namespace Shell.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Shell;V1.0.0.0;component/views/chooseview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Shell;component/views/chooseview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\ChooseView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
